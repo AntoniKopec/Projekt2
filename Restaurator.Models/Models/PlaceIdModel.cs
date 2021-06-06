0339d201f9a6a731ad6b56bddc3680af914652cd
d0nat <szy.szymalski@gmail.com>
2021-06-06 21:14:07 +0200

    Dodanie PlaceIdModel.

---
+++ Restaurator.Models/Models/PlaceIdModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurator.Models.Models
+{
+    class PlaceIdModel
+    {
+        public string PlaceId { get; set; }
+    }
+}
