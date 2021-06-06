516735de90cfee14d44bde4dfa4ae6e03a5ceb21
d0nat <szy.szymalski@gmail.com>
2021-06-06 21:13:14 +0200

    Dodanie PlaceDetailsModel.

---
+++ Restaurator.Models/Models/PlaceDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurator.Models.Models
+{
+    class PlaceDetailsModel
+    {
+        public string Name { get; set; }
+        public string Vicinity { get; set; }
+        public string Rating { get; set; }
+        public string Website { get; set; }
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+        public List<string> Opening_hours { get; set; }
+    }
+}
